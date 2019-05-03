34cedef2f4554308195033c692f7135f414b4283
Lukeveille <luke@lukeleveille.com>
2019-05-03 12:34:22 -0400

    Initial commit

---
+++ src/db.js
@@ -0,0 +1,5 @@
+export let users = [
+  { id: 1, name: 'John Doe', email: 'john@doemail.com', age: 20 },
+  { id: 2, name: 'Jane Doe', email: 'jane@doemail.com', age: 20 },
+  { id: 3, name: 'Why Doe', email: 'y@doemail.com', age: 20 },
+];
